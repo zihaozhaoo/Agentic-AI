e451f5ae62b8c0bf16719404a97de0d6e913e247
LowenJiang <126883423+LowenJiang@users.noreply.github.com>
2025-10-15 13:51:38 -0700

    Here is the code for OR-Tools cost calculation, input: num-vehicles, requests, request distance matrix etc. Currently the requests are randomly generated dummy requests, which for realism would need to be substituted by drawing randomly from the NYC Taxi requests. There is also a code for querying distance using google map API.

---
+++ src/google_map/gmap.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import googlemaps
+
+
+def get_google_maps_client():
+    api_key = "AIzaSyASdmnEivZx-7u6s8tRQn4UbPZ8E9SDe8Y"
+    if not api_key:
+        print("Missing GOOGLE_MAPS_API_KEY environment variable.", file=sys.stderr)
+        sys.exit(1)
+    return googlemaps.Client(key=api_key)
+
+
+def get_driving_time_text(origin, destination):
+    gmaps_client = get_google_maps_client()
+    directions = gmaps_client.directions(origin, destination, mode="driving")
+    if not directions:
+        print("No route found.", file=sys.stderr)
+        sys.exit(2)
+    leg = directions[0]["legs"][0]
+    return leg["duration"]["text"]
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python gmap.py \"ORIGIN\" \"DESTINATION\"", file=sys.stderr)
+        sys.exit(1)
+    origin_arg = sys.argv[1]
+    destination_arg = sys.argv[2]
+    duration_text = get_driving_time_text(origin_arg, destination_arg)
+    print(duration_text)
---
+++ src/solver/__main__.py
@@ -0,0 +1,25 @@
+import pprint
+import yaml
+import os
+from solver.or_tools import cost_estimator
+from data_gen.sf_dummy import simulation
+from data_gen.nodify import create_network
+
+# Configure Parameters
+config_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'configs', 'data_generation.yaml'))
+with open(config_path, 'r') as f:
+    config = yaml.safe_load(f) or {}
+
+or_config_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'configs', 'oracle.yaml'))
+with open(or_config_path, 'r') as f:
+    or_config = yaml.safe_load(f) or {}
+
+# Simulation and Solver
+requests, index_list = simulation(**config['sf_dummy'])
+enc_net = create_network(requests)
+res = cost_estimator(**or_config['or_tools'], distance_matrix=enc_net['distance'], requests=enc_net['requests'])
+
+# Print Results
+print(requests)
+print(enc_net['map'])
+pprint.pprint(res)
